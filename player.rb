f4d3d51de1d48c86ab5faad0e1e34889168b4674
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-03-29 15:53:47 +0000

    implement game logic

--- player.rb
+++ player.rb
@@ -1,5 +1,18 @@
 class Player
-  # Keeps track of players' life.
-  # Starts each player with 3 lives
-  # Deducts a life for wrong answer
+  attr_reader :name, :life
+
+  def initialize(name)
+    # Starts each player with 3 lives
+    @life = 3
+    @name = name
+  end
+  
+  def wrong
+    # Deducts a life for wrong answer
+    @life -= 1
+  end
+
+  def gameover
+    @life == 0
+  end
 end
--- question.rb
+++ question.rb
@@ -1,4 +1,14 @@
 class Question
-  # Generates a question 
-  # Generates the correct answer for the question
+  attr_reader :num1, :num2
+
+  def self.num1
+    @num1 = rand(1..20)
+  end
+  def self.num2
+    @num2 = rand(1..20)
+  end
+  def self.validate(input)
+    input.to_i == (@num1 + @num2)
+  end
+
 end
